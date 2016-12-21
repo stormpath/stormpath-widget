209b09381bf467eb8d872f9552ab029f7f415f2a
nbarbettini <nathanaelb@gmail.com>
2016-12-21 11:31:29 -0800

    Added more failing tests

--- src/data/uri/uri-parser.js
+++ src/data/uri/uri-parser.js
@@ -4,6 +4,21 @@ class UriParser {
   }
 
   hasParameter = (name) => this._uri.indexOf(name + '=') > -1;
+
+  extract(name) {
+    let result = {
+      original: this._uri,
+      new: '',
+      value: ''
+    };
+
+    let start = this._uri.indexOf(name + '=');
+    if (start === -1) {
+      return result;
+    }
+
+    return result;
+  }
 }
 
 export default UriParser;
--- test/uri-parser-spec.js
+++ test/uri-parser-spec.js
@@ -3,33 +3,80 @@ import UriParser from '../src/data/uri';
 
 describe('UriParser', () => {
   it('should be a constructor', () => {
-    expect(UriParser).to.be.a('function');
+    expect(UriParser).to.be.function;
   });
 
-  it('should create a parser with an undefined uri', () => {
-    let p = new UriParser();
-    expect(p).to.be.an('object');
+  it('should create a parser with an undefined URI', () => {
+    let parser = new UriParser();
+    expect(parser).to.be.an('object');
   });
 
-  it('should create a parser with a uri', () => {
-    let p = new UriParser('http://foo.bar.example');
-    expect(p).to.be.an('object');
+  it('should create a parser with a URI', () => {
+    let parser = new UriParser('http://foo.bar.example');
+    expect(parser).to.be.an('object');
   });
 
   describe('hasParameter()', () => {
-    it('should return false for null uri', () => {
-      let p = new UriParser();
-      expect(p.hasParameter('foo')).to.be.false;
+    it('should return false for null URI', () => {
+      let parser = new UriParser();
+      expect(parser.hasParameter('foo')).to.be.false;
     });
 
-    it('should return false for missing uri part', () => {
-      let p = new UriParser('http://foo');
-      expect(p.hasParameter('bar')).to.be.false;
+    it('should return false for missing URI part', () => {
+      let parser = new UriParser('http://foo');
+      expect(parser.hasParameter('bar')).to.be.false;
     });
 
     it('should return true', () => {
-      let p = new UriParser('http://foo?bar=baz');
-      expect(p.hasParameter('bar')).to.be.true;
+      let parser = new UriParser('http://foo?bar=baz');
+      expect(parser.hasParameter('bar')).to.be.true;
+    });
+  });
+
+  describe('extract()', () => {
+    it('should return the original URI unchanged for no match', () => {
+      let parser = new UriParser('http://foo');
+      expect(parser.extract('bar').original).to.eql('http://foo');
+    });
+
+    it('should return the original URI unchanged for a match', () => {
+      let parser = new UriParser('http://foo?bar=1');
+      expect(parser.extract('bar').original).to.eql('http://foo?bar=1');
+    });
+
+    it('should return a new URI with the parameter removed from the beginning', () => {
+      let parser = new UriParser('http://foo?bar=1&baz=2');
+      let result = parser.extract('bar');
+      expect(result.new).to.eql('http://foo?baz=2');
+      expect(result.value).to.eql('1');
+    });
+
+    it('should return a new URI with the parameter removed from the middle', () => {
+      let parser = new UriParser('http://foo?baz=2&bar=1&qux=3');
+      let result = parser.extract('bar');
+      expect(result.new).to.eql('http://foo?baz=2&qux=3');
+      expect(result.value).to.eql('1');
+    });
+
+    it('should return a new URI with the parameter removed from the end', () => {
+      let parser = new UriParser('http://foo?baz=2&bar=1');
+      let result = parser.extract('bar');
+      expect(result.new).to.eql('http://foo?baz=2');
+      expect(result.value).to.eql('1');
+    });
+
+    it('should remove the query mark if the match is the only parameter', () => {
+      let parser = new UriParser('http://foo?bar=1');
+      let result = parser.extract('bar');
+      expect(result.new).to.eql('http://foo');
+      expect(result.value).to.eql('1');
+    });
+
+    it('should ignore matching names before the query part', () => {
+      let parser = new UriParser('http://foobar?bar=1');
+      let result = parser.extract('bar');
+      expect(result.new).to.eql('http://foobar');
+      expect(result.value).to.eql('1');
     });
   });
 });
