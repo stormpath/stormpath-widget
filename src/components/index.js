58a6b0ca523e029717acaf441019cf1eaba719c2
nbarbettini <nathanaelb@gmail.com>
2016-12-23 14:13:09 -0800

    Add caps lock detection

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export FormFieldComponent from './form-field/form-field';
 export LoginComponent from './login/login';
 export RegistrationComponent from './registration/registration';
 export VerifyEmailComponent from './verify-email/verify-email';
+export CapsLockDetector from './caps-lock-detector/caps-lock-detector';
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,6 +2,8 @@ import utils from '../../utils';
 import view from 'html!./login.html';
 import style from '!style-loader!css-loader!less-loader!./login.less';
 
+import CapsLockDetector from '../caps-lock-detector/caps-lock-detector';
+
 class LoginComponent {
   static id = 'login-component';
   static view = view;
@@ -10,6 +12,7 @@ class LoginComponent {
   fields = [];
   state = 'unknown';
   modal = null;
+  capsWarning = false;
 
   constructor(data) {
     this.userService = data.userService;
@@ -35,6 +38,8 @@ class LoginComponent {
         .then(this.onViewModelLoaded.bind(this))
         .catch(this.onError.bind(this, 'loading_error'));
     });
+
+    new CapsLockDetector(document).on('capslock', this.onCapsLock.bind(this));
   }
 
   onError(state, err) {
@@ -64,6 +69,10 @@ class LoginComponent {
     this.userService.login(username, password)
       .then(this.onAuthenticated.bind(this))
       .catch(this.onError.bind(this, 'login_error'));
+  }
+
+  onCapsLock(state) {
+    this.capsWarning = state;
   }
 }
 
