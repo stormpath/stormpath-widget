54c15eae43d41e742d8afed59f9009205b08d8fb
nbarbettini <nathanaelb@gmail.com>
2017-01-10 21:25:54 -0800

    Only show social login section if > 0 accountStores exist

--- src/view-manager.js
+++ src/view-manager.js
@@ -78,6 +78,7 @@ class ViewManager {
     Rivets.formatters['isnt'] = (a, b) => a !== b;
     Rivets.formatters['in'] = (a, b) => (b || '').split(',').indexOf(a) !== -1;
     Rivets.formatters['gt'] = (x, y) => x > y;
+    Rivets.formatters['any'] = (arr) => (arr || []).length > 0;
     Rivets.formatters.prefix = utils.prefix;
 
     Rivets.binders.required = (el, val) => el.required = val === true;
