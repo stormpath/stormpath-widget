381bbe6c18ea95f733c44cc005d0c2ac44269b9e
Robin <orhedenr@gmail.com>
2017-01-12 19:57:08 +0100

    support email verification

--- src/data/user/cookie-user-service.js
+++ src/data/user/cookie-user-service.js
@@ -90,14 +90,29 @@ class CookieUserService extends EventEmitter {
   verifyEmail(token) {
     return this.httpProvider.getJson('/verify', {
       sptoken: token
+    }).then(() => {
+      this._setState('emailVerified');
     });
   }
 
   register(data) {
     return this.httpProvider.postJson('/register', data).then((result) => {
-      this.account = result.account;
-      this._setState('registered');
-      return Promise.resolve(result.account);
+      const account = this.account = result.account;
+
+      switch (account.status.toLowerCase()) {
+        case 'enabled':
+          this._setState('registered');
+          break;
+
+        case 'unverified':
+          this._setState('emailVerificationRequired');
+          break;
+
+        default:
+          return Promise.reject(new Error('Account returned unknown status \'' + account.status + '\'.'));
+      }
+
+      return Promise.resolve(account);
     });
   }
 
