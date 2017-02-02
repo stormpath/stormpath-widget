4fd3033190fbaa11102af1bb4f2e14144bfc5a8d
nbarbettini <nathanaelb@gmail.com>
2017-02-01 19:33:42 -0800

    Update dev server for new example page location

--- dev-server.js
+++ dev-server.js
@@ -7,7 +7,7 @@ var app = express();
 var compiler = webpack(config);
 var port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/example/login'));
+app.use('/', express.static(__dirname + '/example'));
 
 app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
