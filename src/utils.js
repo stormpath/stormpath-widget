53a0354911b0d6b19a064f78b0b31f118276635c
Robert <robert@stormpath.com>
2017-02-03 11:28:04 -0800

    Refactor
    * decodeURIComponent will remove spaces for you
    * I don’t see why we needed the separate _handleSocialProviderCallbackError ?
    * re-phrase this as a provider login error, because this flow will also be handling saml and OIDC callbacks

--- src/utils.js
+++ src/utils.js
@@ -134,7 +134,6 @@ class Utils {
       }
 
       try {
-        value = (value || '').replace(/\+/gim, '%20'); // Convert + to %20.
         value = decodeURIComponent(value);
       } catch (e) {
         value = undefined;
