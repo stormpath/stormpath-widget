b493fbdedab356fa38a8805a1767c6a32a35c395
nbarbettini <nathanaelb@gmail.com>
2017-02-14 11:38:43 -0800

    Detect the current git branch

--- publish.js
+++ publish.js
@@ -3,24 +3,43 @@
 const semver = require('semver');
 const fs = require('fs');
 const child_process = require('child_process');
-
-const packageJsonPath = `${__dirname}/package.json`;
+const git = require('git-rev-sync');
 
 const version = process.argv[2];
-if (!semver.valid(version)) {
-  throw new Error(`Version '${version}' is not a valid semver string!`);
-}
+const packageJsonPath = `${__dirname}/package.json`;
+
+let throwIfVersionInvalid = (versionArg) => {
+  if (!semver.valid(versionArg)) {
+    throw new Error(`Version '${versionArg}' is not a valid semver string!`);
+  }
+};
+
+let throwIfNotOnMaster = () => {
+  const branch = git.branch();
+  if (branch !== 'master') {
+    throw new Error(`Must be on branch master (current branch is ${branch})`);
+  }
+};
+
+let updatePackageJsonVersion = (path, newVersion) => {
+  const packageInfo = JSON.parse(fs.readFileSync(path, 'utf-8'));
+  packageInfo.version = newVersion;
+  fs.writeFileSync(path, JSON.stringify(packageInfo, null, 2), 'utf-8');
+};
+
+let buildProject = () => {
+  const buildResult = child_process.spawnSync('npm', ['run', 'build']);
+  if (buildResult.status !== 0) {
+    throw new Error('Build failed');
+  }
+};
+
+throwIfVersionInvalid(version);
+throwIfNotOnMaster();
 
 /* eslint-disable no-console */
 console.log(`Prepping version ${version}`);
+updatePackageJsonVersion(packageJsonPath, version);
 
-const packageInfo = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-packageInfo.version = version;
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageInfo, null, 2), 'utf-8');
-
-// console.log('Building project...');
-// const buildResult = child_process.spawnSync('npm', ['run', 'build']);
-// if (buildResult.status !== 0) {
-//   throw new Error('Build failed');
-// }
-
+console.log('Building project...');
+buildProject();
