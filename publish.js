2557feb52e1af5377d52f69f70f98cdf6e20393a
nbarbettini <nathanaelb@gmail.com>
2017-02-17 15:12:44 -0800

    Compute hash and write to integrity file

--- publish.js
+++ publish.js
@@ -4,9 +4,12 @@ const semver = require('semver');
 const fs = require('fs');
 const child_process = require('child_process');
 const git = require('git-rev-sync');
+const chalk = require('chalk');
 
 const version = process.argv[2];
 const packageJsonPath = `${__dirname}/package.json`;
+const minifiedDistPath = `${__dirname}/dist/stormpath.min.js`;
+const integrityFilePath = `${__dirname}/integrity.txt`;
 
 let throwIfVersionInvalid = (versionArg) => {
   if (!semver.valid(versionArg)) {
@@ -41,14 +44,31 @@ let buildProject = () => {
   }
 };
 
+let computeHashFor = (path) => {
+  return child_process
+    .execSync(`cat ${path} | openssl dgst -sha384 -binary | openssl enc -base64 -A`)
+    .toString();
+};
+
+let appendToIntegrityFile = (version, hash) => {
+  fs.appendFileSync(integrityFilePath, `${version} ${hash}`);
+};
+
+/* eslint-disable no-console */
 throwIfVersionInvalid(version);
 //throwIfNotOnMaster();
 console.log('TODO RESTORE');
 throwIfVersionLessThanCurrentTag(version);
-
-/* eslint-disable no-console */
-console.log(`Prepping version ${version}`);
 updatePackageJsonVersion(packageJsonPath, version);
 
 console.log('Building project...');
 buildProject();
+
+const minifiedHash = computeHashFor(minifiedDistPath);
+console.log(`Built stormpath.min.js v${version} with SHA ${minifiedHash}`);
+// TODO - update README.MD automatically
+appendToIntegrityFile(version, minifiedHash);
+
+// commit and tag
+
+console.log(chalk.green('Done! Don\'t forget to run:\ngit push origin --tags'));
